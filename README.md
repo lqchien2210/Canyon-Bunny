7d918127bd87c6a0e2717c68610c17505625235f
Chien <drpacman2468@gmail.com>
2021-09-16 15:17:25 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Canyon-Bunny
+# CayonBunny
---
+++ core/src/com/packtpub/libgdx/canyonbunny/WorldController.java
@@ -0,0 +1,13 @@
+package com.packtpub.libgdx.canyonbunny;
+
+public class WorldController {
+	private static final String TAG =
+	        WorldController.class.getName();
+	public WorldController() {}
+	private void init() {}
+	public void update(float deltaTime) {}
+	public void update(float deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+}
